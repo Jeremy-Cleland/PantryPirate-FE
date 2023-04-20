9405e1a5003b1f6228c4b33489b72ccdded8729f
Jordan Covington <Atlantafan1087@gmail.com>
2023-04-19 17:04:41 -0700

    Added Styling for MyList Screen

--- screens/EditList.js
+++ screens/EditList.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Button, TextInput } from 'react-native';
+import { View, Button, TextInput, StyleSheet, Text } from 'react-native';
 import axios from 'axios';
 
 export default function EditList({ navigation, route }) {
@@ -36,22 +36,51 @@ export default function EditList({ navigation, route }) {
   }
   return (
     <View>
-      <TextInput
-        placeholder="List Name"
-        value={listName}
-        onChangeText={setListName}
-      />
-      <TextInput
-        placeholder="Add Members"
-        value={members}
-        onChangeText={setMembers}
-      />
-      <Button title="Update List" onPress={handleUpdateSubmit} />
-      {route.params.list.creator === 'testUser' &&
-        <Button title="Delete List" onPress={handleDeleteSubmit} />
-      }
+      <View style={styles.delete} >
+        {route.params.list.creator === 'testUser' &&
+          <Button title="Delete List" onPress={handleDeleteSubmit} />
+        }
+      </View>
+      <View style={{marginTop: 50}}>
+        <Text style={{marginLeft: 30, marginTop: 30}}>List Name</Text>
+        <View style={styles.container}>
+          <TextInput
+            placeholder="List Name"
+            value={listName}
+            onChangeText={setListName}
+          />
+        </View>
+        <Text style={{marginLeft: 30, marginTop: 30}}>Members</Text>
+        <View style={styles.container}>
+          <TextInput
+            placeholder="Add Members"
+            value={members}
+            onChangeText={setMembers}
+          />
+        </View>
+      </View>
+      <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+        <Button title="Update List" onPress={handleUpdateSubmit} />
+
+
+      </View>
     </View>
   )
 
 
 }
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderColor: 'black',
+    margin: 10,
+    padding: 10,
+  },
+  delete: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+  }
+
+});
